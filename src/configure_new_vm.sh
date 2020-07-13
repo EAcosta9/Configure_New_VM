bbd37323bb020751db01afcd41e3e7b4405fc1e0
eacosta9 <emanuelacosta91197@gmail.com>
2020-07-12 21:01:37 -0400

    Added license header and created notes directory

--- src/configure_new_vm.sh
+++ src/configure_new_vm.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+###############################################################################
+
+# Change History 
+# July-12-2020 - Emanuel Acosta - Creation
+
+###############################################################################
+###############################################################################
+###############################################################################
+# Copyright (c) 2020, Emanuel Acosta
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+################################################################################
+################################################################################
+################################################################################
+
+
 cd $HOME
 ## tell bash that it should exit the script if any statement returns a non-true return value
 set -e
